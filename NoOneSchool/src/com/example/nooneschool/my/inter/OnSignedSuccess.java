ca9f8c2a29a9e45d5f7ba0b76a801773449f0335
fycnb <1162054822@qq.com>
2018-12-09 14:33:27 +0800

    test

--- NoOneSchool/src/com/example/nooneschool/my/inter/OnSignedSuccess.java
+++
@@ -1,5 +0,0 @@
-package com.example.nooneschool.my.inter;
-
-public interface OnSignedSuccess {
-	void OnSignedSuccess();
-}
--- NoOneSchool/src/com/example/nooneschool/util/ListItemClickHelp.java
+++
@@ -1,7 +0,0 @@
-package com.example.nooneschool.util;
-
-import android.view.View;
-
-public interface ListItemClickHelp {
-	void onClick(View item, View widget, int position, int which);
-}
